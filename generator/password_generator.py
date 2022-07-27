ad8a79fc15597f197db4293e665230788861dc0c
Oscar Anyiam <oscaranyiam94@gmail.com>
2022-07-27 16:11:06 +0100

    password gen works

--- generator/password_generator.py
+++ generator/password_generator.py
@@ -11,11 +11,12 @@ class PasswordGenerator:
     def generate_alphabets(self):
         listOfAlphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
                            "s", "t",
-                           "u", "v", "v", "w", "x", "y", "z"]
+                           "u", "v", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L",
+                           "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
         alp_res = ""
         for a in listOfAlphabets:
-            ranOne = random.randint(0, len(listOfAlphabets) - 1)
-            alp_res = alp_res + listOfAlphabets[ranOne]
+            ranOne = random.choice(listOfAlphabets)
+            alp_res += ranOne
             if len(alp_res) == self.numOfAlphabets:
                 break
         return alp_res
@@ -27,8 +28,8 @@ class PasswordGenerator:
             print("Recommend special characters to increase password strength")
             return char_res
         for a in listOfChars:
-            ran = random.randint(0, len(listOfChars) - 1)
-            char_res = char_res + listOfChars[ran]
+            ran = random.choice(listOfChars)
+            char_res += ran
             if len(char_res) == self.numOfChars:
                 break
         return char_res
@@ -40,8 +41,8 @@ class PasswordGenerator:
             return num_res
         listOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         for a in listOfNumbers:
-            ran = random.randint(0, len(listOfNumbers) - 1)
-            num_res = num_res + str(listOfNumbers[ran])
+            ran = random.choice(listOfNumbers)
+            num_res += str(ran)
             if len(num_res) == self.numOfDigits:
                 break
         return num_res
@@ -50,8 +51,13 @@ class PasswordGenerator:
         alphabets = self.generate_alphabets()
         numbers = self.generate_numbers()
         characters = self.generate_chars()
-        ans = f"{alphabets} {numbers} {characters}"
-        split_res = ans.split(" ")
-        for x in split_res:
-          fin = "".join(split_res)
-        return fin
+        total = len(alphabets) + len(numbers) + len(characters)
+        ans = f"{alphabets}{numbers}{characters}"
+        password = ""
+        arr = []
+        for n in ans:
+            arr.append(n)
+        random.shuffle(arr)
+        for i in arr:
+            password += i
+        return password
